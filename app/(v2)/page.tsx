fcb0579c0dfa5b5434e0a4ec8bc4d66faa5edaa3
Namit <rnamit099@gmail.com>
2026-05-07 23:22:00 +0530

    Integrate SignalGridSection into main page layout

--- app/(v2)/page.tsx
+++ app/(v2)/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from './components/sections/HeroSection'
 import ProjectsSection from './components/sections/ProjectsSection'
 import AboutSection from './components/sections/AboutSection'
+import SignalGridSection from './components/sections/SignalGridSection'
 import ContactSection from './components/sections/ContactSection'
 
 export default function V2Page() {
@@ -8,6 +9,7 @@ export default function V2Page() {
     <main>
       <HeroSection />
       <AboutSection />
+      <SignalGridSection />
       <ProjectsSection />
       <ContactSection />
     </main>
